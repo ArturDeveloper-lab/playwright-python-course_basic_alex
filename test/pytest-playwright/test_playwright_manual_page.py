2d1a48ff14c0d36480ca7b804ce7341512a82c6d
ArturBoltyansky <artur.boltiansky79@gmail.com>
2024-04-13 11:11:09 +0300

    first commit

---
+++ test/pytest-playwright/test_playwright_manual_page.py
@@ -0,0 +1,26 @@
+import pytest
+from playwright.sync_api import Page, Playwright
+
+
+@pytest.fixture(scope='function')
+def setup(playwright: Playwright) -> Page:
+    # browser = playwright.chromium.launch(headless=True, slow_mo=500, args=["--start-maximized"])
+    browser = playwright.chromium.launch(headless=False, slow_mo=500)
+    page = browser.new_page()
+
+    page.goto("https://playwright.dev/python/")
+
+    return page
+
+
+def test_playwright_python_docs(setup):
+    page = setup
+    page.locator('[class="navbar__item navbar__link"]', has_text='API').click()
+    page.wait_for_timeout(3000)
+
+
+def test_playwright_python_api(setup):
+    page = setup
+    page.get_by_role(role='link', name='Docs').click()
+
+    page.wait_for_timeout(3000)
